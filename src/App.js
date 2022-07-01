1366e1c8cdd9b9a3ff9cbcb027605550e1ae7ec0
essaid madi <essaid.madi@yahoo.com>
2022-07-01 12:13:47 +0200

    finishing the sidbar and adding the darkmode

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { DarkTheme, LightTheme } from "./utils/Theme";
+
+
+const Container = styled.div`
+display: flex
+`;
+const Main = styled.div`
+ flex: 7;
+ background-color: ${({theme}) => theme.bg};
+`;
+const Wrapper = styled.div``;
 function App() {
+  const [Darkmode, setDarkmode] = useState(true);
   return (
-    <div>
-      Hello world!
-    </div>
+    <ThemeProvider theme={Darkmode ? DarkTheme : LightTheme}>
+    <Container>
+        <Menu Darkmode = {Darkmode} setDarkmode = {setDarkmode}/>
+        <Main>
+          <Navbar/>
+          <Wrapper>
+            video Cards
+          </Wrapper>
+        </Main>
+    </Container>
+    </ThemeProvider>
   );
 }
 
